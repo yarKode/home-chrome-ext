6fade730c04616c4e3b0b21dd5ee46ca64124cb9
yarkode <grigoriy.yarko@gmail.com>
2021-07-28 22:37:34 +0300

    backgorund fetched from unsplash

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const body = document.getElementById("body");
 
+async function getAndSetImage() {
+  try {
+    const res = await fetch(
+      ` https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature`
+    );
+
+    const {
+      urls: { regular: img },
+    } = await res.json();
+
+    body.style.backgroundImage = `url('${img}')`;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getAndSetImage();
