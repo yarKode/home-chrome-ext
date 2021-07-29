05ef74230b7aca48955020347d9c5454c3feaa0e
yarkode <grigoriy.yarko@gmail.com>
2021-07-29 14:53:02 +0300

    author of img displayed

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const body = document.getElementById("body");
+const author = document.querySelector(".author");
 
 async function getAndSetImage() {
   try {
     const res = await fetch(
       ` https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature`
     );
+    /* 
+    const data = await res.json();
+
+    console.log(data); */
 
     const {
       urls: { regular: img },
+      user: { name },
     } = await res.json();
 
     body.style.backgroundImage = `url('${img}')`;
+    author.insertAdjacentText("afterbegin", name);
   } catch (err) {
     console.error(err);
   }
