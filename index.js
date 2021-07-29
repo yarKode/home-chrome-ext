69495a570f03e76dc7922aabd0e86ebd8835f135
yarkode <grigoriy.yarko@gmail.com>
2021-07-29 17:00:39 +0300

    set default background if query in fetch request is incorrect

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ async function getAndSetImage() {
     author.insertAdjacentText("afterbegin", `By: ${name}`);
   } catch (err) {
     console.error(err);
+    body.style.backgroundImage = `url("https://images.unsplash.com/photo-1547955922-26be0c1600c4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Mjc1NjcwNTU&ixlib=rb-1.2.1&q=80&w=1080")`;
+    author.insertAdjacentText("afterbegin", `By: Anton Lochov`);
   }
 }
 
